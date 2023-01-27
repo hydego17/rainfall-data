5760444d29c3c99f85a5390dae4b3dc39474e7de
Umma Ahimsha <uahimsha@gmail.com>
2023-01-27 16:29:24 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Button, TextInput } from '@mantine/core';
+import Head from 'next/head';
 import { useForm } from 'react-hook-form';
 
 import { BsFilePdf, BsFileEarmarkExcel } from 'react-icons/bs';
@@ -11,37 +12,43 @@ export default function Home() {
       ['to']: '31-01-2021',
       ['parameter[]']: 'rainfall',
       ['type']: 'mkg',
-      ['format']: 'xls',
     },
   });
 
   return (
-    <main className='py-8 container'>
-      <h1 className='text-3xl font-bold'>Data BMKG Nadhirah Lamak Syamba</h1>
+    <>
+      <Head>
+        <title>Nadhirah Lamak Syamba</title>
+      </Head>
 
-      <hr />
+      <main className='py-8 container'>
+        <h1 className='text-3xl font-bold'>Data BMKG Nadhirah Lamak Syamba</h1>
 
-      <div className='border p-4 max-w-[500px]'>
-        <form action='https://dataonline.bmkg.go.id/data_iklim/download' method='post' target='_blank'>
-          <div className='space-y-2'>
-            <TextInput label='Station Name' {...register('station-name')} />
-            <TextInput label='Start Date (MM-DD-YYYY)' {...register('from')} />
-            <TextInput label='End Date (MM-DD-YYYY)' {...register('to')} />
-            <TextInput label='Parameter' {...register('parameter[]')} />
-          </div>
+        <hr />
 
-          <hr />
+        <div className='border p-4 max-w-[500px]'>
+          <form action='https://dataonline.bmkg.go.id/data_iklim/download' method='post' target='_blank'>
+            <div className='space-y-2'>
+              <TextInput label='Station Name' {...register('station-name')} />
+              <TextInput label='Start Date (MM-DD-YYYY)' {...register('from')} />
+              <TextInput label='End Date (MM-DD-YYYY)' {...register('to')} />
+              <TextInput label='Parameter' {...register('parameter[]')} />
+              <TextInput label='Type' {...register('type')} disabled />
+            </div>
 
-          <div className='flex gap-2'>
-            <Button type='submit' variant='filled' name='format' value='xls' leftIcon={<BsFileEarmarkExcel />}>
-              XLS
-            </Button>
-            <Button type='submit' variant='outline' name='format' value='pdf' leftIcon={<BsFilePdf />}>
-              PDF
-            </Button>
-          </div>
-        </form>
-      </div>
-    </main>
+            <hr />
+
+            <div className='flex gap-2'>
+              <Button type='submit' variant='filled' name='format' value='xls' leftIcon={<BsFileEarmarkExcel />}>
+                XLS
+              </Button>
+              <Button type='submit' variant='outline' name='format' value='pdf' leftIcon={<BsFilePdf />}>
+                PDF
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
